8236f22aac8afd7be5a0af56692c618601ad23e2
Sergio Vera <sergio.vera@splendia.com>
2013-10-25 13:01:51 +0200

    Added interests foreign key

---
+++ app/database/migrations/2013_10_25_123614_AddInterestsForeignKey.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddInterestsForeignKey extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table)
+        {
+            $table
+                ->integer("interest_id")
+                ->unsigned()
+                ->nullable();
+            $table
+                ->foreign('interest_id')
+                ->references('id')
+                ->on('interests');                
+        });
+	}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+           	Schema::dropColumn('interest_id');
+        });
+    }
+
+}
