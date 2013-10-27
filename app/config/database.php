e468354e02421942aeef8f38fa40df9827701a29
Sergio Vera <sergio.vera@gmail.com>
2013-10-27 10:33:17 +0100

    Back to Pagodabox settings

--- app/config/database.php
+++ app/config/database.php
@@ -45,16 +45,44 @@ return array(
     */
 
     'connections' => array(
+
+        'sqlite' => array(
+            'driver'   => 'sqlite',
+            'database' => __DIR__.'/../database/production.sqlite',
+            'prefix'   => '',
+        ),
+
         'mysql' => array(
                 'driver'    => 'mysql',
-                'host'      => isset($_SERVER['APP_NAME']) ? 'svera.mysql.eu1.frbit.com' : 'localhost',
-                'database'  => isset($_SERVER['APP_NAME']) ? 'svera' : '203web',
-                'username'  => isset($_SERVER['APP_NAME']) ? 'svera' : 'root',
-                'password'  => isset($_SERVER['APP_NAME']) ? 'BZqNJ0Pb0BytRMDY' : '',
+                'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+                'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : '203web',
+                'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'root',
+                'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : '',
                 'charset'   => 'utf8',
                 'collation' => 'utf8_unicode_ci',
                 'prefix'    => '',
-        )
+        ),
+
+        'pgsql' => array(
+            'driver'   => 'pgsql',
+            'host'     => 'localhost',
+            'database' => 'database',
+            'username' => 'root',
+            'password' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ),
+
+        'sqlsrv' => array(
+            'driver'   => 'sqlsrv',
+            'host'     => 'localhost',
+            'database' => 'database',
+            'username' => 'root',
+            'password' => '',
+            'prefix'   => '',
+        ),
+
     ),
 
     /*
